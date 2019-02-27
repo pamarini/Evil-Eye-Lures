0c054b902d30d7a5a00499868dc14704d231ce0a
Peter Marinis <31866648+pamarini@users.noreply.github.com>
2019-02-27 11:02:38 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/*
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}*/
+
+/*
+var nav = document.getElementById('nav');
+window.addEventListener("scroll", function(event) {
+            if(window.pageYOffset>100){
+
+                nav.style.background = "#007bff";
+
+            }
+            else{
+                nav.style.background = "transparent";
+            }
+        });
+      
+        */
