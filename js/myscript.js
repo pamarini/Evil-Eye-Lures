28e82f9a7de508aa0d78ca97a9438bdcedcda130
Peter Marinis <peter.marinis96@gmail.com>
2019-03-11 15:42:06 -0400

    add to cart popup

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {
+  if (window.pageYOffset>300){
+    nav.style.background = "#FddFFF80";
+     //nav.style.opacity = .4;
+    }else{
+     nav.style.background = "transparent";
+   }
+ }
+
